9b908bba39472bbf646ec1523269cb2fc716a188
奉嘉 <qwer14788lll@163.com>
2020-10-09 16:30:04 +0800

    项目第三阶段完成（垂直瀑布布局）

--- app/src/main/java/com/example/recyclerviewdemo/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewdemo/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.recyclerviewdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,19 +14,31 @@ class MainActivity : AppCompatActivity() {
     private fun initFruits() {
         repeat(2)
         {
-            fruitList.add(Fruit("鼠", R.drawable.animal_0))
-            fruitList.add(Fruit("牛", R.drawable.animal_1))
-            fruitList.add(Fruit("虎", R.drawable.animal_2))
-            fruitList.add(Fruit("兔", R.drawable.animal_3))
-            fruitList.add(Fruit("龙", R.drawable.animal_4))
-            fruitList.add(Fruit("蛇", R.drawable.animal_5))
-            fruitList.add(Fruit("马", R.drawable.animal_6))
-            fruitList.add(Fruit("羊", R.drawable.animal_7))
-            fruitList.add(Fruit("猴", R.drawable.animal_8))
-            fruitList.add(Fruit("鸡", R.drawable.animal_9))
-            fruitList.add(Fruit("狗", R.drawable.animal_10))
-            fruitList.add(Fruit("猪", R.drawable.animal_11))
+            fruitList.add(Fruit(getRandomLengthString("鼠"), R.drawable.animal_0))
+            fruitList.add(Fruit(getRandomLengthString("牛"), R.drawable.animal_1))
+            fruitList.add(Fruit(getRandomLengthString("虎"), R.drawable.animal_2))
+            fruitList.add(Fruit(getRandomLengthString("兔"), R.drawable.animal_3))
+            fruitList.add(Fruit(getRandomLengthString("龙"), R.drawable.animal_4))
+            fruitList.add(Fruit(getRandomLengthString("蛇"), R.drawable.animal_5))
+            fruitList.add(Fruit(getRandomLengthString("马"), R.drawable.animal_6))
+            fruitList.add(Fruit(getRandomLengthString("羊"), R.drawable.animal_7))
+            fruitList.add(Fruit(getRandomLengthString("猴"), R.drawable.animal_8))
+            fruitList.add(Fruit(getRandomLengthString("鸡"), R.drawable.animal_9))
+            fruitList.add(Fruit(getRandomLengthString("狗"), R.drawable.animal_10))
+            fruitList.add(Fruit(getRandomLengthString("猪"), R.drawable.animal_11))
         }
+    }
+
+    /**
+     * 生成随机拷贝数量的字符串
+     */
+    private fun getRandomLengthString(string: String): String {
+        val n = (1..20).random()
+        val builder = StringBuilder()
+        repeat(n) {
+            builder.append(string)
+        }
+        return builder.toString()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +46,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //数据初始化
         initFruits()
-        //创建线性布局管理器
-        val layoutManager = LinearLayoutManager(this)
-        //设置线性方向为横向
-        layoutManager.orientation = LinearLayoutManager.HORIZONTAL
+        //创建瀑布布局管理器
+        val layoutManager = StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL)
         //设置布局管理器
         RecyclerViewTest.layoutManager = layoutManager
         //实例化数据适配器，装载数据
